3acdbe56af02d38c941ad8b2fdb0263e51eed4fb
AlyaNurAmalia27 <alyanuramaliamarhas>
2024-09-26 09:14:08 +0700

    Semangat kata akumah

--- lib/page2.dart
+++ lib/page2.dart
@@ -2,12 +2,49 @@ import 'package:flutter/material.dart';
 
 class Page2 extends StatelessWidget {
   const Page2({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('ini halaman 2'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+        ),
+        body:Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget> [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container (
+            width: 100,
+            height: 100,
+            color: Color.fromARGB(255, 209, 56, 45)
+            ),
+            Container (
+            width: 100,
+            height: 100,
+            color: Color.fromARGB(255, 54, 143, 216)
+             ), 
+            ],
+             ),
+             Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+            Container (
+            width: 100,
+            height: 100,
+            color: Color.fromARGB(255, 153, 128, 219)
+             ), 
+            Container(
+            width: 100,
+            height: 100,
+            color: Color.fromARGB(255, 127, 156, 209)
+             ),
+              ],
+             ),
+          ],
+           ),
+        ),
       ),
     );
   }
